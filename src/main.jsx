7c2629df6c1cc5eb062cec5827f741f5727f90d3
Akash <akash.enlighten@gmail.com>
2025-10-11 17:22:33 -0700

    added cercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { Analytics } from '@vercel/analytics/next';
+
 import './index.css'
 
 import App from './App.jsx'
@@ -7,5 +10,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
